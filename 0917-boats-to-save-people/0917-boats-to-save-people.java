e8697d788d99c7c506fbf4824b3deafab810dd2a
HARINI J <harinijagadeesh17@gmail.com>
2025-06-18 16:03:44 +0530

    Time: 16 ms (95.25%), Space: 54.2 MB (90.54%) - LeetHub

--- 0917-boats-to-save-people/0917-boats-to-save-people.java
+++ 0917-boats-to-save-people/0917-boats-to-save-people.java
@@ -1,23 +1,17 @@
 class Solution {
     public int numRescueBoats(int[] people, int limit) {
-        Arrays.sort(people);        
+        Arrays.sort(people);
         int boats = 0;
         int l = 0;
-        int r = people.length - 1; 
-       while(l<=r)
-       {
-        if(people[l]+people[r]<=limit)
-        {
-            boats++;
-            l++;
-            r--;
-        }
-        else if(people[l]+people[r]>limit)
-        {
+        int r = people.length - 1;
+
+        while (l <= r) {
+            if (people[l] + people[r] <= limit) {
+                l++;
+            }
             r--;
             boats++;
         }
-       }
-       return boats;
+        return boats;
     }
 }
