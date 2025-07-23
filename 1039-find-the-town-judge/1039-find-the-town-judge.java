58f895cfdd1bfd1ac3f81a788d6ef878d21c8b10
HARINI J <harinijagadeesh17@gmail.com>
2025-07-23 16:03:45 +0530

    Time: 2 ms (99.31%), Space: 54.8 MB (7.67%) - LeetHub

---
+++ 1039-find-the-town-judge/1039-find-the-town-judge.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        int judge[]= new int[n+1];
+        for(int a[] :trust){
+            judge[a[0]]--;
+            judge[a[1]]++;
+        }
+        for(int i =1;i<=n;i++){
+            if(judge[i]==n-1) return i;
+        }
+        return -1;
+    }
+}
